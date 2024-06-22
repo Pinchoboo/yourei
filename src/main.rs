e18f054f4b62541dea59329371aca61a2a75b647
user <pinchoboo@gmail.com>
2024-06-22 13:53:29 +0200

    soft rewrite 1.5

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use regex::Regex;
 use scraper::{Html, Selector};
-use std::{fmt::Display, io::Read, mem};
+use std::{fmt::Display, io::Read};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -37,10 +37,7 @@ fn main() {
         .read_to_string(&mut page_content)
         .unwrap();
 
-    let mut examples = extract_examples(&page_content);
-    format_examples(&cli, &mut examples);
-
-    for e in examples {
+    for e in extract_examples(&page_content, cli) {
         println!("{e}\n");
     }
 }
@@ -53,97 +50,99 @@ struct Example {
     source: Option<String>,
 }
 
-impl Example {
-    fn map<F: Fn(String) -> String>(&mut self, f: F) {
-        self.prev = mem::take(&mut self.prev).map(&f);
-        self.sentence = mem::take(&mut self.sentence).map(&f);
-        self.next = mem::take(&mut self.next).map(&f);
-        self.source = mem::take(&mut self.source).map(&f);
-    }
-}
-
 impl Display for Example {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "{}{}{}\n{}",
+            "{}{}{}",
             &self.prev.as_deref().unwrap_or_default(),
             &self.sentence.as_deref().unwrap_or_default(),
-            &self.next.as_deref().unwrap_or_default(),
-            &self.source.as_deref().unwrap_or_default(),
-        )
+            &self.next.as_deref().unwrap_or_default()
+        )?;
+        if let Some(source) = &self.source {
+            write!(f, "\n{source}")?;
+        }
+        Ok(())
     }
 }
 
-fn extract_examples(html_content: &str) -> Vec<Example> {
+const UNDERLINE: &str = "\x1b[4m";
+const NOLINE: &str = "\x1b[24m";
+const GREEN: &str = "\x1b[32m";
+const RESET: &str = "\x1b[0m";
+
+fn extract_examples(html_content: &str, cli: Cli) -> Vec<Example> {
+    let rt_regex = Regex::new(r"<rt>(?<reading>.*?)<\/rt>").unwrap();
+    let word_regex = word_regex(&cli.word);
+    let kanji_only_regex = kanji_only_regex(&cli.word);
+
+    let reading_format = if cli.furigana {
+        format!("{UNDERLINE}$reading{NOLINE}")
+    } else {
+        "".to_string()
+    };
+    let word_in_green = format!("{GREEN}$word{RESET}");
+
+    let html_to_ansi = |mut s: String| {
+        s = s.replace("<ruby>", "").replace("</ruby>", "");
+        let s = rt_regex.replace_all(&s, &reading_format);
+        if cli.emphasize {
+            if word_regex.is_match(&s) {
+                word_regex.replace_all(&s, &word_in_green)
+            } else {
+                kanji_only_regex.replace_all(&s, &word_in_green)
+            }
+        } else {
+            s
+        }
+        .to_string()
+    };
+
     Html::parse_document(html_content)
         .select(&Selector::parse("ul.sentence-list > [id^=\"sentence-\"]").unwrap())
-        .filter_map(|example| {
+        .map(|example| {
             let inner_html = |query| {
                 example
                     .select(&Selector::parse(query).unwrap())
                     .next()
-                    .map(|a| a.inner_html())
+                    .map(|a| html_to_ansi(a.inner_html()))
             };
-            Some(Example {
+            Example {
                 prev: inner_html(".prev-sentence"),
                 sentence: inner_html(".the-sentence"),
                 next: inner_html(".next-sentence"),
                 source: inner_html(".sentence-source-title > *"),
-            })
+            }
         })
         .collect()
 }
 
-fn format_examples(cli: &Cli, examples: &mut [Example]) {
-    let underline = "\x1b[4m";
-    let noline = "\x1b[24m";
-    let green = "\x1b[32m";
-    let reset = "\x1b[0m";
-
-    let any_underlined = format!("{underline}.*?{noline}").replace('[', "\\[");
-
-    let rt_tag = Regex::new(r"<rt>(?<reading>.*?)<\/rt>").unwrap();
-    let mut word_pattern = cli.word.chars().fold(r"(?<word>".to_string(), |acc, c| {
-        format!(r"{acc}{c}({any_underlined})?",)
-    }) + ")";
-    let word_finder = Regex::new(&word_pattern).unwrap();
-    word_pattern = Regex::new(r"(\p{Script=Katakana}|\p{Script=Hiragana})")
-        .unwrap()
-        .replace_all(&cli.word, "")
-        .chars()
-        .fold("(?<word>".to_string(), |acc, c| {
+fn word_regex(word: &str) -> Regex {
+    Regex::new(
+        &(word.chars().fold(r"(?<word>".to_string(), |acc, c| {
             format!(
-                r"{acc}{c}({})?({any_underlined})?",
-                (r"(\p{Script=Katakana}|\p{Script=Hiragana})*?")
+                r"{acc}{c}({})?",
+                format!("{UNDERLINE}.*?{NOLINE}").replace('[', "\\[")
             )
-        })
-        + ")";
-    let kanji_only_finder = Regex::new(&word_pattern).unwrap();
+        }) + ")"),
+    )
+    .unwrap()
+}
 
-    for e in examples {
-        e.map(|s| s.replace("<ruby>", "").replace("</ruby>", ""));
-        if cli.furigana {
-            e.map(|s| {
-                rt_tag
-                    .replace_all(&s, format!("{underline}$reading{noline}"))
-                    .to_string()
-            });
-        } else {
-            e.map(|s| rt_tag.replace_all(&s, "").to_string());
-        }
-        if cli.emphasize {
-            e.map(|s| {
-                if word_finder.is_match(&s) {
-                    word_finder
-                        .replace_all(&s, format!("{green}$word{reset}"))
-                        .to_string()
-                } else {
-                    kanji_only_finder
-                        .replace_all(&s, format!("{green}$word{reset}"))
-                        .to_string()
-                }
-            });
-        }
-    }
+fn kanji_only_regex(word: &str) -> Regex {
+    Regex::new(
+        &(Regex::new(r"(\p{Script=Katakana}|\p{Script=Hiragana})")
+            .unwrap()
+            .replace_all(word, "")
+            .chars()
+            .fold("(?<word>".to_string(), |acc, c| {
+                format!(
+                    r"{acc}{c}({})?({})?",
+                    r"(\p{Script=Katakana}|\p{Script=Hiragana})*?",
+                    format!("{UNDERLINE}.*?{NOLINE}").replace('[', "\\[")
+                )
+            })
+            + ")"),
+    )
+    .unwrap()
 }
